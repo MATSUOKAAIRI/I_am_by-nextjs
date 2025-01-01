28ef18595056b297c8d85153f0ab5f73151b993e
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-02 00:37:36 +0900

    refactor:pastの削除

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import style1 from './page.module.css'
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import About from "@/components/about/about";
-import Past from "@/components/past/past";
 import Continue from "@/components/continue/continue"
 
 
@@ -14,7 +13,6 @@ export default function Home() {
     <div className={style1.s} >AIRI　MATSUOKA</div>
     <Header/>
     <About/>
-    <Past/>
     <Continue/>
     <Footer/>
   </>
