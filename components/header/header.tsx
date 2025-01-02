aa6080704691480cc7fe6d0ec1d4b506449ab478
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-03 02:08:52 +0900

    headerの更新

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,14 +1,14 @@
-import style1 from './header.module.css';
+import style from './header.module.css';
 
 export default function Header(){
     return(<> 
-        <nav className={style1.d}>
-            <ul className={style1.gnavi}>
-                <li><a href="#Top" className="no-underline text-yellow-200">top</a></li>
-                <li><a href="#About" className="no-underline text-blue-200">about</a></li>
-                <li><a href="#Past" className="no-underline text-green-200">past</a></li>
-                <li><a href="#Continue"  className="no-underline text-red-300">continue</a></li>
+        <header className="justify-center bg--black text-[40px]">
+        <div className="text-white text-[80px] ">　AIRI　MATSUOKA</div>
+            <ul className={style.gnavi}>
+                <li><a href="#Header" className="no-underline text-yellow-200">Top</a></li>
+                <li><a href="#About" className="no-underline text-blue-200">About</a></li>
+                <li><a href="#Do"  className="no-underline text-red-300">Do</a></li>
             </ul>
-        </nav>
+        </header>
     </>)
 }
