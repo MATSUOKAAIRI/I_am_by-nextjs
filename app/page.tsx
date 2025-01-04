10dfcfbe596076cb17c9b18884d7e506fd493cee
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-04 11:13:48 +0900

    update:新画面作成に伴うpageの更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image";
 import "../styles/globals.css";
-import style1 from './page.module.css'
+import Hero from "@/components/hero/hero";
 import Header from "@/components/header/header";
 import Footer from "@/components/footer/footer";
 import About from "@/components/about/about";
+import University from "@/components/university/university";
 import Do from "@/components/do/do"
 
 
 export default function Home() {
   return (
   <>
-  <div id="Header" className=""></div>
+  
+  <div id="Header"></div>
+  <Hero/>
    <Header/>
-    <div id="About" className=""></div>
+    <div id="About"></div>
     <About/>
-    <div id="Do" className=""></div>
+    <div id="University"></div>
+    <University/>
+    <div id="Do"></div>
     <Do/>
     <Footer/>
   </>
