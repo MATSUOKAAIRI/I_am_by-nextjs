0a8fc56db85c298a79f54d0d5063b908151bf657
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-04 11:17:39 +0900

    update:about画面の更新

--- components/about/about.tsx
+++ components/about/about.tsx
@@ -1,31 +1,32 @@
 import style1 from './about.module.css';
+import Image from 'next/image';
 
 export default function About(){
     return( 
-    <div><h3 className="text-center text-[100px] ">自己紹介</h3>
-    <div className={style1.tapebox}>
-    <div className={style1.tape}></div>
-        <p className={style1.p}>名前:</p>
-        <h4 className={style1.h4}>松岡愛梨</h4>
-    </div>
+<div  className="flex items-center justify-center bg-green-200 mt-8 mb-16">
+    <div className='bg-yellow-100 text-gray-700 rounded-3xl p-16 items-center max-w-6xl h-5/6 w-full justify-center'>
+        <h1 className="text-7xl font-bold mb-6">About Me</h1>
+        <div className='flex justify-center mb-4 underline decoration-red-800 underline-offset-8'>
+            <img src="./アイコン.jpg" alt="hero" width={90} height={90} className="rounded-3xl items-start"/>
+            <p className="text-[50px] ">名前:　</p>
+            <h4 className="text-center text-[50px]">りぃ</h4>
+        </div>
      
-        <div className={style1.tapebox2}>
-        <div className={style1.tape2}></div>
-            <div><p className={style1.p}>出身</p></div>
-            <h4 className={style1.h4}>富山県</h4><img src="istockphoto-842082264-612x612.jpg" alt="景色" width="150" height="150" className={style1.float}/><img src="toyama_7_kencho-mei_g.png" alt="富山地図" width="150" height="150" className={style1.imgright}/>
-        </div>     
-        
 
-    <div className={style1.tapebox}>
-    <div className={style1.tape}></div>
-        <p className={style1.p}>趣味</p>
-        <h4 className={style1.h4}>動画視聴、読書</h4>
+
+        <div className='flex justify-center mb-4 underline decoration-red-800 underline-offset-8 left-10'>
+            <p className="text-[50px] mt-.5">出身:　</p>
+            <h4 className="text-center text-[50px]">富山県</h4>
+        </div>
+           <div className='flex justify-center mb-4 underline decoration-red-800 underline-offset-8'>
+            <p className="text-[50px] mt-.5 ">好きな食べ物:　</p>
+            <h4 className="text-center text-[50px]">ラーメン</h4>
+        </div>
+        <div className='flex justify-center mb-1 underline decoration-red-800 underline-offset-8'>
+            <p className="text-[50px] mt-.5">趣味:　</p>
+            <h4 className="text-center text-[50px]">動画視聴、読書</h4>
+        </div>
     </div>
-
-    <div className={style1.tapebox}>
-    <div className={style1.tape}></div>
-    <p className={style1.p}>やりたいこと</p>
-    <h4 className={style1.h4}>バイト、旅行</h4>
-    </div></div> 
+</div>
     )
 }
