c8049f47f9cd76ae375430d32943a9889e8b681a
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-04 11:19:55 +0900

    update:headerとfooterの更新

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,12 +2,12 @@ import style from './header.module.css';
 
 export default function Header(){
     return(<> 
-        <header className="justify-center bg--black text-[40px]">
-        <div className="text-white text-[80px] ">　AIRI　MATSUOKA</div>
+        <header className="justify-center bg-green-200 text-[40px] z-10 border-b-2 border-white  mx-auto max-w-3xl">
             <ul className={style.gnavi}>
-                <li><a href="#Header" className="no-underline text-yellow-200">Top</a></li>
-                <li><a href="#About" className="no-underline text-blue-200">About</a></li>
-                <li><a href="#Do"  className="no-underline text-red-300">Do</a></li>
+                <li><a href="#Header" className="no-underline text-yellow-400">Top</a></li>
+                <li><a href="#About" className="no-underline text-blue-400">About</a></li>
+                <li><a href="#University"  className="no-underline text-green-400">University</a></li>
+                <li><a href="#Do"  className="no-underline text-red-400">Do</a></li>
             </ul>
         </header>
     </>)
