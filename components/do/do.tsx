04ceae237d4f3ec27986f504e42eeebc9d79c6a2
MATSUOKAAIRI <c1419948@st.kanazawa-it.ac.jp>
2025-01-05 01:12:41 +0900

    update:レスポンシブ対応

--- components/do/do.tsx
+++ components/do/do.tsx
@@ -2,17 +2,17 @@ import style1 from './do.module.css';
 
 export default function Do(){
     return(
-        <div className="flex items-center justify-center bg-green-200 mb-16 mt-8"> 
-            <div className='bg-yellow-100 text-gray-700 rounded-3xl p-16 items-center max-w-6xl h-5/6 w-full justify-center'>
+        <div className="flex items-center justify-center bg-green-200 mt-8 md:mb-12 mb-16"> 
+            <div className='bg-yellow-100 text-gray-700 rounded-3xl md:p-16 items-center max-w-6xl h-11/12 w-11/12 '>
 
-                <h1 className="text-7xl font-bold mb-6">Doing</h1>
-                <div className=' justify-center mb-4 underline decoration-red-800 underline-offset-8'>
-                <h4 className="text-[50px] ">興味分野:</h4>
-                <p className="text-[50px] text-center">セキュリティ、Webフロントエンド</p>
+                <h1 className="md:text-7xl text-5xl font-bold mb-6">Doing</h1>
+                <div className='justify-center mb-4 underline decoration-red-800 underline-offset-8'>
+                <h4 className="md:text-[50px] text-[20px]">興味分野:</h4>
+                <p className="md:text-[50px] text-[20px] text-center">セキュリティ、Webフロントエンド</p>
                 </div>
                 <div className=' justify-center mb-4 underline decoration-red-800 underline-offset-8'>
-                <h4 className="text-[50px] ">言語:</h4>
-                <p className="text-[50px] text-center">python、Next.js、react、HTML、CSS、JavaScript</p>
+                <h4 className="md:text-[50px] text-[20px]">言語:</h4>
+                <p className="md:text-[50px] text-[20px] text-center">python、Next.js、react、HTML、CSS、JavaScript</p>
                 </div>
             </div>
         </div>
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,12 +2,12 @@ import style from './header.module.css';
 
 export default function Header(){
     return(<> 
-        <header className="justify-center bg-green-200 text-[40px] z-10 border-b-2 border-white  mx-auto max-w-3xl">
-            <ul className={style.gnavi}>
-                <li><a href="#Header" className="no-underline text-yellow-400">Top</a></li>
-                <li><a href="#About" className="no-underline text-blue-400">About</a></li>
-                <li><a href="#University"  className="no-underline text-green-400">University</a></li>
-                <li><a href="#Do"  className="no-underline text-red-400">Do</a></li>
+        <header className="bg-green-200 text-[40px] z-10 border-b-2 border-white  mx-auto md:max-w-3xl p-4">
+            <ul className={`${style.gnavi} flex flex-col md:flex-row justify-center items-center`}>
+                <li className="mb-2 md:mb-0 md:mr-4"><a href="#Header" className="no-underline text-yellow-400">Top</a></li>
+                <li className="mb-2 md:mb-0 md:mr-4"><a href="#About" className="no-underline text-blue-400">About</a></li>
+                <li className="mb-2 md:mb-0 md:mr-4"><a href="#University"  className="no-underline text-green-400">University</a></li>
+                <li className="mb-2 md:mb-0 "><a href="#Do"  className="no-underline text-red-400">Do</a></li>
             </ul>
         </header>
     </>)
