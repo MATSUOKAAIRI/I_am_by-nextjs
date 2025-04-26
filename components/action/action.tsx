738deb44fde98cee40888de8ba42e4c425e17c74
MATSUOKAAIRI <aMatsu20191121@gmail.com>
2025-04-26 20:06:51 +0900

    a

--- components/action/action.tsx
+++ components/action/action.tsx
@@ -3,8 +3,8 @@ return(
 <div  className="flex items-center justify-center bg-green-200 mt-8 mb-16">
     <div className='bg-yellow-100 text-gray-700 rounded-3xl md:p-16 max-w-6xl h-5/6 w-11/12 justify-center'>
         <div className="flex ml-6">
-        <h1 className="md:text-7xl text-5xl font-bold mb-6">Action</h1>
-        <h1 className="md:text-6xl text-4xl font-bold mb-6">(参加イベント)</h1>
+            <h1 className="md:text-7xl text-5xl font-bold mb-6">Action</h1>
+            <h1 className="md:text-6xl text-4xl font-bold mb-6">(参加イベント)</h1>
         </div>
         <div className='flex justify-center mb-4 underline decoration-red-800 underline-offset-8'>
             <p className="md:text-[40px] text-[20px]">2024年度:　</p>            
